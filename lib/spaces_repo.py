897dd58fbe4d933a2e2400b3c840e98ec32f2817
Adam Hussain <adam.hussain001@gmail.com>
2024-11-04 15:14:08 +0000

    updating file

--- lib/spaces_repo.py
+++ lib/spaces_repo.py
@@ -11,3 +11,20 @@ class SpacesRepo:
     def remove_space(self, space_id):
         if space_id in self.spaces:
             del self.spaces[space_id]
+    
+    def list_spaces(self):
+        return list(self.spaces.values())
+
+    def find_available_spaces(self, start_date, end_date):
+        available_spaces = []
+        for space in self.spaces.values():
+            if space.is_available(start_date, end_date):
+                available_spaces.append(space)
+        return available_spaces
+
+    def book_space(self, space_id, start_date, end_date):
+        space = self.get_space(space_id)
+        if space and space.is_available(start_date, end_date):
+            space.book(start_date, end_date)
+            return True
+        return False
