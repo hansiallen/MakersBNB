5088d8818e8b2d1946d268e467cadc0be66af504
Alexandre Monoyioudis <alexandremonoyioudis@gmail.com>
2024-11-07 21:16:46 +0000

    Ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- lib/database_connection.py
+++ lib/database_connection.py
@@ -9,8 +9,9 @@ from psycopg.rows import dict_row
 # That's why we have provided it!
 class DatabaseConnection:
     # VVV CHANGE BOTH OF THESE VVV
-
     DEV_DATABASE_NAME = "makersbnb"
+    if os.getenv('GITHUB_ACTIONS') == 'true':
+       DEV_DATABASE_NAME = "makersbnb_test" 
     TEST_DATABASE_NAME = "makersbnb_test"
 
     def __init__(self, test_mode=False):
